48a8ca5a7071c388213064a84d4ff443ba381cea
Nidal Jaafar <nidaljaafar.nj@gmail.com>
2023-04-03 20:32:02 +0300

    first commit

---
+++ src/main/java/platform/media/contentdelivery/ContentDeliveryApplication.java
@@ -0,0 +1,13 @@
+package platform.media.contentdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/platform/media/contentdelivery/ContentDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.media.contentdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
